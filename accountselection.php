eb9884308d01c89ec751beaf25ce04ca9d123b55
AlexandreMeledandri <alexandre.meledandri@gmail.com>
2018-06-06 14:36:48 +0200

    Alex

---
+++ accountselection.php
@@ -0,0 +1,12 @@
+<?php 
+$bdd = new PDO("mysql:host=localhost;dbname=petitcomptable;charset = uft8", "root", "root");
+?>
+<ul class="list-group" style="width: 20%;">
+  <?php
+    $req = $bdd->query("SELECT label FROM accounts WHERE iduser = 1");
+    while ($result = $req->fetch()) { ?> 
+        <li class="list-group-item list-group-item-action"><a href="index.php?accunts=<?php echo $result['label'] ?>"><?php echo $result['label']; ?></a></li>
+        <?php
+    }
+    ?>
+</ul>
