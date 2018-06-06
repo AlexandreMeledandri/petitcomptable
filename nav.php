02b6c64590ac489c3637930a4e66f8db039d5ccd
Grandpierre Adrien <adrien.grandpierre@edu.itescia.fr>
2018-06-06 15:50:06 +0200

    o

--- nav.php
+++ nav.php
@@ -1,6 +1,6 @@
 <nav class="nav navbar-expand-lg navbar-light bg-light">
   <a class="nav-link" href="#">Home</a>
   <a class="nav-link" href="#">Bank Accounts</a>
-  <a class="nav-link" href="#">Profile</a>
+  <a class="nav-link" href="profile.php">Profile</a>
   <a class="nav-link" href="index.php?logout=true">Déconnexion</a>
 </nav>
