d8b1983f529edb3d61af1f4684907f54a8565b0c
AlexandreMeledandri <alexandre.meledandri@gmail.com>
2018-06-06 16:56:01 +0200

    commit Alex

--- index.php
+++ index.php
@@ -1,8 +1,4 @@
 <?php
-    session_start();
-
-    echo $_SESSION['idUser'];
-
     if(isset($_GET['logout'])) {
        setcookie("lastname", "", time() - 3600, "/");
        setcookie("firstname", "", time() - 3600, "/");
@@ -28,9 +24,3 @@
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js" integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm" crossorigin="anonymous"></script>
     </body>
 </html>
-
-<?php
-
-if(isset($_POST['createAccount'])){
-    var_dump($_POST['name']);
-}
