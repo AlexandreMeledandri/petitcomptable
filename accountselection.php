7d8f93be751073fc16f67cbe739f5abd01ca2c84
AlexandreMeledandri <alexandre.meledandri@gmail.com>
2018-06-07 17:07:18 +0200

    commit Alex

--- accountselection.php
+++ accountselection.php
@@ -1,9 +1,11 @@
-<ul class="list-group" style="width: 20%;">
-  <?php
-    $req = $db->query("SELECT id, label FROM accounts WHERE iduser = 1");
-    while ($result = $req->fetch()) { ?> 
-        <li class="list-group-item list-group-item-action"><a href="index.php?accounts=<?php echo $result['id'] ?>"><?php echo $result['label']; ?></a></li>
-        <?php
-    }
-    ?>
-</ul>
+<div class="col-md-4">
+    <ul class="list-group">
+    <?php
+        $req = $db->query("SELECT id, label FROM accounts WHERE iduser = 1");
+        while ($result = $req->fetch()) { ?> 
+            <li class="list-group-item list-group-item-action"><a href="index.php?accounts=<?php echo $result['id'] ?>"><?php echo $result['label']; ?></a></li>
+            <?php
+        }
+        ?>
+    </ul>
+</div>
--- operations.php
+++ operations.php
@@ -8,32 +8,34 @@ $operations = $query->fetchAll(PDO::FETCH_ASSOC);
 $labels = array_keys($operations[0]);
 
 ?>
-<table class="table table-bordered">
-    <thead>
-    <tr>
-    <?php
-    foreach($labels as $label){
-        echo '<th scope="col">';
-        echo $label;
-        echo '</th>';
-    }
-    ?>
-    </tr>
-    </thead>
-    <tbody>
-    <?php
-    $values = array_map(function($value){
-        return array_map(function($val){return $val;},$value);
-    
-    },$operations);
-    foreach($operations as $key => $value){
-        echo '<tr>';
-            foreach($value as $subKey => $subValue) {
-                echo "<td>" . $subValue . "</td>";
-            }
-    
-        echo '</tr>';
-    }
-    ?>
-    </tbody>
-</table>
+<div class="col-md-8">
+    <table class="table table-bordered">
+        <thead>
+        <tr>
+        <?php
+        foreach($labels as $label){
+            echo '<th scope="col">';
+            echo $label;
+            echo '</th>';
+        }
+        ?>
+        </tr>
+        </thead>
+        <tbody>
+        <?php
+        $values = array_map(function($value){
+            return array_map(function($val){return $val;},$value);
+        
+        },$operations);
+        foreach($operations as $key => $value){
+            echo '<tr>';
+                foreach($value as $subKey => $subValue) {
+                    echo "<td>" . $subValue . "</td>";
+                }
+        
+            echo '</tr>';
+        }
+        ?>
+        </tbody>
+    </table>
+</div>
