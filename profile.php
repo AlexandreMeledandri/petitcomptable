1f9af4dfadd2e805fcd183b331aca5129dccfe53
Grandpierre Adrien <adrien.grandpierre@edu.itescia.fr>
2018-06-07 10:17:02 +0200

    o

--- profile.php
+++ profile.php
@@ -10,6 +10,28 @@
     <div>
         <?php include_once "nav.php"; ?>
     </div>
+    <form method="POST" action="">
+        <input type="text" name="firstname" placeholder="FirstName" value="
+        <?php
+        $req = $bdd->query("SELECT * FROM user");
+        $result = $req->fetch();
+        echo $result['firstname'];
+        ?>">
+        <input type="text" name="lastname" placeholder="LastName" value="
+        <?php
+        $req = $bdd->query("SELECT * FROM user");
+        $result = $req->fetch();
+        $result['lastname'];
+        ?>">
+        <input type="text" name="psw" placeholder="password" value="
+        <?php 
+        $req = $bdd->query("SELECT * FROM user");
+        $result = $req->fetch();
+        $result['psw'];
+        ?>">
+        <input type="submit" value="Add User !">
+    </form>
+    
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js" integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm" crossorigin="anonymous"></script>
 
