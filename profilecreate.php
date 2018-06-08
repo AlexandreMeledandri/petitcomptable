36d919eb5fcad0a429d1af9d8cde1222f5289ee1
Grandpierre Adrien <adrien.grandpierre@edu.itescia.fr>
2018-06-08 10:33:02 +0200

    prof

--- profilecreate.php
+++ profilecreate.php
@@ -6,18 +6,26 @@
             <label for="label">Label : </label>
             <input type="text" name="label" id="label" maxlength="35" />
             <label for="type">Type : </label>
-            <select id="type">
+            <select id="type" name="type">
                 <option value="current">Current</option>
                 <option value="saving">Savings</option>
                 <option value="joint">Joint</option>
             </select>
             <label for="currency">Currency : </label>
-            <select id="currency">
+            <select id="currency" name="currency">
                 <option value="euro">Euro</option>
                 <option value="dollar">Dollar</option>
             </select>
             <label for="balance">Initial Balance : </label>
             <input type="text" name="balance" id="balance" maxlength="10" />
+            <input type="submit" value="Add account">
     </form>
+    <?php 
+    var_dump($_POST);
+        if(isset($_POST['balance']) && isset($_POST['currency']) && isset($_POST['label']) && isset($_POST['type']))
+        {
+            $req = $db->prepare("INSERT INTO accounts(iduser, balance, currency, label, type) VALUES(?, ?, ?, ?, ?)");
+            $req->execute(array($_SESSION['idUser'], $_POST['balance'], $_POST['currency'], $_POST['label'], $_POST['type']));
+        }
+    ?>
 </div>
-<?php
